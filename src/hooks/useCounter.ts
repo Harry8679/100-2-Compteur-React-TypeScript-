fd52653b98bfd3c252d48dea9898e5efb455cb14
Harry7986 <emarh.harry.code@gmail.com>
2026-02-08 07:25:39 +0100

    Use Counter 3

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import { HistoryEntry } from '../types';
+import type { HistoryEntry } from '../types';
 
 interface UseCounterProps {
   initialValue?: number;
