a6bc7f48384752261e1f636cd69fa6ef5b93990b
Harry7986 <emarh.harry.code@gmail.com>
2026-02-09 14:26:22 +0100

    Add deployment configuration 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: 'https://github.com/Harry8679/100-2-Compteur-React-TypeScript-'
+  base: '/100-2-Compteur-React-TypeScript-'
 })
